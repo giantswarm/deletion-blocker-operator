b7d962bf11115485ce0b4d482836dd7412fd96c2
Marian Steinbach <marian@giantswarm.io>
2025-08-19 10:23:49 +0200

    Change container registry to gsoci.azurecr.io (#207)
    * Change container registry to gsoci.azurecr.io
    
    * Update CHANGELOG.md

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Changed
+
+- Change container image registry in Makefile to `gsoci.azurecr.io`
+
 ## [0.3.1] - 2024-11-07
 
 ### Changed
--- Makefile.kubebuilder.mk
+++ Makefile.kubebuilder.mk
@@ -1,6 +1,6 @@
 
 # Image URL to use all building/pushing image targets
-IMG ?= quay.io/erkanerolgs/deletion-blocker-operator:v2
+IMG ?= gsoci.azurecr.io/erkanerolgs/deletion-blocker-operator:v2
 
 # ENVTEST_K8S_VERSION refers to the version of kubebuilder assets to be downloaded by envtest binary.
 ENVTEST_K8S_VERSION = 1.23
